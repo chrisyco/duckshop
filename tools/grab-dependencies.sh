07645c880f30be352806fad796f35c63ad4e1f5e
Chris Wong <chrisyco@gmail.com>
2011-06-29 14:04:47 +1200

    Used Bukkit's Maven repo instead of installing it manually

--- tools/grab-dependencies.sh
+++ tools/grab-dependencies.sh
@@ -8,8 +8,8 @@ cd $TMPDIR
 
 echo "Chris Wong's Super Awesome Dependency Downloader!" | tee "$LOG"
 
-name=$(download 'http://ci.bukkit.org/job/dev-Bukkit/lastSuccessfulBuild/artifact/target/bukkit-0.0.1-SNAPSHOT.jar')
-install "Bukkit" $name org.bukkit bukkit 0.0.1-SNAPSHOT
+#name=$(download 'http://ci.bukkit.org/job/dev-Bukkit/lastSuccessfulBuild/artifact/target/bukkit-0.0.1-SNAPSHOT.jar')
+#install "Bukkit" $name org.bukkit bukkit 0.0.1-SNAPSHOT
 
 name=$(download 'https://github.com/iConomy/Register/raw/master/dist/Register.jar')
 install "Register" $name com.iConomy Register LATEST
